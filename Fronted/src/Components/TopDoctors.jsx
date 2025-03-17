ad7119ed354572998aa0085569539d96a45f1d0a
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-03-17 13:51:17 +0530

    commited

--- Fronted/src/Components/TopDoctors.jsx
+++ Fronted/src/Components/TopDoctors.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { doctors } from "../assets/assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 
 const TopDoctors = () => {
 
   const navigate = useNavigate();
+  const [isExpanded, setIsExpanded] = useState(10);
+
+  function showmore() {
+    setIsExpanded((prev) => prev + 6);
+  }
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-[#262626] md:mx-10">
@@ -13,12 +18,12 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
 
-      <div className="w-full grid grid-cols-2  lg:grid-cols-3 gap-4 mt-5 overflow-scroll pt-4">
-        {doctors.map((item, index) => (
+      <div className="w-full grid grid-cols-2  lg:grid-cols-4 gap-4 mt-5 overflow-scroll pt-4">
+        {doctors.slice(0, isExpanded).map((item, index) => (
           <div
             onClick={() => navigate(`/appointment/${item._id}`)}
             key={index}
-            className="border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            className="border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-7px] transition-all duration-500"
           >
             <img className="bg-[#EAEFFF]" src={item.image} alt="speciality" />
             <div className="p-4">
@@ -33,14 +38,23 @@ const TopDoctors = () => {
         ))}
       </div>
 
-      <Link
-        onClick={() => window.scrollTo(0, 0)}
-        to="/doctors"
-        className="flex items-center gap-2 bg-primary px-6 md:px-8 py-3 rounded-md text-white text-sm font-medium shadow-md hover:scale-105 transition-transform duration-300"
-      >
-        See More
-      </Link>
-      
+      <div className="mt-10">
+        {
+          isExpanded < doctors.length ? (
+            <svg onClick={showmore} className="size-6 text-violet-500 cursor-pointer animate-bounce" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>) : (
+
+            <Link
+              onClick={() => window.scrollTo(0, 0)}
+              to="/doctors"
+              className="flex items-center gap-2 bg-primary px-6 md:px-8 py-3 rounded-md text-white text-sm font-medium shadow-md hover:scale-105 transition-transform duration-300"
+            >
+              See More
+            </Link>
+            )
+           
+        }
+      </div>
+
     </div>
   );
 };
